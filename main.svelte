d3b1c432130fdcf4b5b8f13963133d42a8ad2b60
rempixel <yjiang00634@gmail.com>
2025-09-26 19:47:14 -0400

    Made extremely simple drawing canvas to mimic what I had done in a2 but also fulfills creative coding assignment.

---
+++ main.svelte
@@ -0,0 +1,19 @@
+<script>
+import Canvas from './canvas.svelte';
+
+const background = '#fff'
+
+</script>
+
+<main>
+    <Canvas {background}/>
+</main>
+
+<style>
+    main {
+        max-width: 300px;
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem 0;
+	}
+</style>
